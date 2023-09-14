55cdb025bb4bf7d7ed1be2bea73b19b5a7438105
Jason Shi <jshigoodies@gmail.com>
2023-09-14 12:04:21 -0500

    added sound to shooting rail gun

--- Model Assets/rail gun.gd
+++ Model Assets/rail gun.gd
@@ -1,14 +1,13 @@
 extends Spatial
 
 var bullet_scene = preload("res://Model Assets/bullet.tscn")
-var is_charged= false
-
+var is_charged = false
 func _ready():
 	$ChargeTimer.connect("timeout", self, "charge_timer_timeout")
 
 
 func _input(event):
-	if event.is_action_pressed("right_mouse_button"):
+	if event.is_action_pressed("right_mouse_button") and !is_charged:
 		$ChargeTimer.start()
 	if event.is_action_released("right_mouse_button") and is_charged:
 		is_charged = false
@@ -30,4 +29,6 @@ func shoot():
 	bullet.set_global_transform($GunMuzzle.get_global_transform())
 	
 func charge_timer_timeout():
+	$ChargeTimer.stop()
 	is_charged = true
+	# print("FIRE FIRE FIRE")
