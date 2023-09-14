be8161dd65c843f132364cf7f63e80dee888b472
Jason Shi <jshigoodies@gmail.com>
2023-09-14 17:14:34 -0500

    added bullet lifetime

--- Model Assets/bullet.gd
+++ Model Assets/bullet.gd
@@ -4,8 +4,12 @@ extends RigidBody
 var bullet_speed = 50.0  # Adjust the bullet's speed as needed.
 
 func _ready():
-	pass  # Optional setup code here.
+	$LifetimeBullet.connect("timeout", self, "_lifetime_end")
 
 func set_direction(direction):
 	# Set the bullet's initial linear velocity in the given direction.
 	linear_velocity = direction.normalized() * bullet_speed
+
+func _lifetime_end():
+	$LifetimeBullet.stop()
+	queue_free()
